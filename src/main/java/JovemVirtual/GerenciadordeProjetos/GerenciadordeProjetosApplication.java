0b36788178737b73a3c50e07788b580f3006d025
saviofreitass <saviofreitas04@gmail.com>
2024-09-19 13:39:12 -0300

    first commit

---
+++ src/main/java/JovemVirtual/GerenciadordeProjetos/GerenciadordeProjetosApplication.java
@@ -0,0 +1,13 @@
+package JovemVirtual.GerenciadordeProjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordeProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordeProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JovemVirtual/GerenciadordeProjetos/GerenciadordeProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package JovemVirtual.GerenciadordeProjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordeProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
